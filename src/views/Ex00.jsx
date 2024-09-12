bf75da093cf21f0128abdefb0d1d2b6a0336b500
H-JUYEONG <dud9902@gmail.com>
2024-09-12 17:37:32 +0900

    2024.09.12

---
+++ src/views/Ex00.jsx
@@ -0,0 +1,15 @@
+//import 라이브러리
+import React from "react";
+
+//import 컴포넌트
+//import css
+
+const Ex00 = () => {
+  
+  return (
+    <>
+      <p>안녕하세요</p>
+    </>
+  );
+};
+export default Ex00;
---
+++ src/views/Ex01.jsx
@@ -0,0 +1,13 @@
+//import 라이브러리
+import React from "react";
+
+const Ex01 = () => {
+
+  return (
+    <div>
+      <p>안녕하세요2</p>
+      <p>안녕하세요2</p>
+    </div>
+  );
+};
+export default Ex01;
